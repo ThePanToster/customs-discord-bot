8bb860ce987293e6e74dacfe8cdbbed2e8cf1404
ThePanToster <alcrafter@o2.pl>
2023-01-06 02:47:45 +0100

    Say command refactor

---
+++ commands/say.js
@@ -0,0 +1,34 @@
+const{ SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    locales: {
+        pl: 'Witaj ',
+    },
+    data: new SlashCommandBuilder()
+        .setName('say')
+        .setNameLocalizations({
+            pl: 'powiedz',
+        })
+        .setDescription('The bot will repeat after you')
+        .setDescriptionLocalizations({
+            pl: 'Bot mówi co chcesz',
+        })
+        .addStringOption(option =>
+            option
+                .setName('input')
+                .setNameLocalizations({
+                    pl: 'tekst',
+                })
+                .setDescription('The input to echo back')
+                .setDescriptionLocalizations({
+                    pl: 'Wiadomość, którą ma przekazać bot',
+                })),
+    async execute(interaction) {
+        try{
+            await interaction.reply(`${interaction.options._hoistedOptions[0].value}`);
+        }
+        catch{
+            await interaction.reply((this.locales[interaction.locale] ?? 'Hello ') + interaction.user.username);
+        }
+    },
+};
