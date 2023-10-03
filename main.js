7455df9619971178dd938873418c7fa18663bd1f
ThePanToster <alcrafter@o2.pl>
2023-10-03 21:39:57 +0200

    Add voice states permission

--- main.js
+++ main.js
@@ -1,64 +1,69 @@
 // Importing classes and declaring variables
 require('dotenv').config();
-const{ Client, Events, GatewayIntentBits, Collection } = require('discord.js');
+const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
 const fs = require('fs');
 const path = require('node:path');
 const token = process.env.TOKEN;
 const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMembers,
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildVoiceStates,
+  ],
+  presence: {
+    activities: [
+      {
+        name: '/help',
+        type: 2,
+      },
     ],
-    presence: {
-        activities: [{
-            name: '/help',
-            type: 2,
-        }],
-    },
+  },
 });
-
 
 // Loading all commands
 client.commands = new Collection();
 const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith('.js'));
 
-for(const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
-    if('data' in command && 'execute' in command)
-        client.commands.set(command.data.name, command);
-    else
-        console.log(`[UWAGA] Polecenie ${filePath} nie posiada własności "data" lub "execute".`);
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  if ('data' in command && 'execute' in command)
+    client.commands.set(command.data.name, command);
+  else
+    console.log(
+      `[UWAGA] Polecenie ${filePath} nie posiada własności "data" lub "execute".`
+    );
 }
 
-
 // When ready
-client.once(Events.ClientReady, c => {
-    console.log(`Bot pomyslnie zalogowany jako ${c.user.tag}`);
+client.once(Events.ClientReady, (c) => {
+  console.log(`Bot pomyslnie zalogowany jako ${c.user.tag}`);
 });
-
 
 // Command handler
-client.on(Events.InteractionCreate, async interaction => {
-    if(!interaction.isChatInputCommand()) return;
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
 
-    const command = interaction.client.commands.get(interaction.commandName);
+  const command = interaction.client.commands.get(interaction.commandName);
 
-    if(!command) {
-        console.error(`Nie znaleziono polecenia ${interaction.commandName}.`);
-        return;
-    }
+  if (!command) {
+    console.error(`Nie znaleziono polecenia ${interaction.commandName}.`);
+    return;
+  }
 
-    try{
-        await command.execute(interaction);
-    }
-    catch(error) {
-        console.error(error);
-        await interaction.reply({ content: 'Wystąpił problem w uruchomieniu tego polecenia!', ephemeral: true });
-    }
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({
+      content: 'Wystąpił problem w uruchomieniu tego polecenia!',
+      ephemeral: true,
+    });
+  }
 });
-
 
 // Log in via token
 client.login(token);
