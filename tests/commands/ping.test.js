6e52b5ebb52b301c8918098f8b94b73d237ec990
ThePanToster <alcrafter@o2.pl>
2024-08-16 12:00:53 +0200

    Fix ping.js test

--- tests/commands/ping.test.js
+++ tests/commands/ping.test.js
@@ -2,7 +2,7 @@ const ping = require("../../commands/ping");
 const mockInteraction = {
   returnValue: "",
   client: { ws: { ping: 100 } },
-  createdTimestamp: Date.now(),
+  createdTimestamp: 1723802300257,
   reply(message) {
     this.returnValue = message;
   },
@@ -12,7 +12,9 @@ describe("Ping command", () => {
   it('returns "pong!"', () => {
     ping.execute(mockInteraction);
     expect(mockInteraction.returnValue.embeds[0].description).toEqual(
-      "🏓 Pong!\n> Latency: **3**ms\n> API Latency: **100**ms"
+      `🏓 Pong!\n> Latency: **${
+        Date.now() - 1723802300257
+      }**ms\n> API Latency: **100**ms`
     );
   });
 });
