5080ad3c9e01188c26d3e66ce87ca98a205aef62
ThePanToster <alcrafter@o2.pl>
2024-08-16 11:47:29 +0200

    Fix ping.js unit test

--- tests/commands/ping.test.js
+++ tests/commands/ping.test.js
@@ -1,12 +1,18 @@
-const ping = require('../../commands/ping');
+const ping = require("../../commands/ping");
 const mockInteraction = {
-    returnValue: "",
-    reply(message){this.returnValue = message}
+  returnValue: "",
+  client: { ws: { ping: 100 } },
+  createdTimestamp: Date.now(),
+  reply(message) {
+    this.returnValue = message;
+  },
 };
 
-describe('Ping command', () => {
-    it('returns "pong!"', () => {
-        ping.execute(mockInteraction);
-        expect(mockInteraction.returnValue.embeds[0].description).toEqual('pong!');
-    });
+describe("Ping command", () => {
+  it('returns "pong!"', () => {
+    ping.execute(mockInteraction);
+    expect(mockInteraction.returnValue.embeds[0].description).toEqual(
+      "🏓 Pong!\n> Latency: **3**ms\n> API Latency: **100**ms"
+    );
+  });
 });
